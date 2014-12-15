83783a7ebcecd4b4d40f898dbcdc02a04c25d588
gorpmlabs <gorpmlabs@gmail.com>
2014-12-15 14:37:51 -0700

    Create conf.py

---
+++ API_Friday/12122014_geonames/code/conf.py
@@ -0,0 +1,11 @@
+# geonames username
+username    = demo
+
+# max rows to return for cities
+maxpplrows  = 100
+
+# minimum population
+population_min = 99
+
+# feet to meters conversion
+ft_to_meters = 3.2808399
